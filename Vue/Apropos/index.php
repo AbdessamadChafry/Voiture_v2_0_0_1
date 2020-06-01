c85459f4fa0922ba9bd0b807d0642de001d2792b
AbdessamadChafry <abdou.chafry@gmail.com>
2020-05-31 21:09:49 -0400

    Dépot initiale

---
+++ Vue/Apropos/index.php
@@ -0,0 +1,33 @@
+<?php $titre = 'Le Blogue de Abdessamad Chafry version 1.0.0.1'; ?>
+
+<ul>
+    <li>Abdessamad Chafry</li>
+
+    <li>420-4A4 MO Web et Bases de données</li>
+    <li>Hiver 2020, Collège Montmorency</li>
+</ul>
+        <li>
+            <form action="<?= $utilisateur != '' ? 'Admin' : ''; ?>commentaires" method="post">
+                <input type="submit" value="Changer de controleur d'accueil">
+            </form>
+        </li>
+    </ul>
+    
+<table>
+    <tr>
+        <td>
+            <h4>Base de données utilisée par l'application :</h4>
+            <object data="Contenu/images/table.png" type="image/svg+xml" height="500" width="800">
+                <img src="Contenu/images/table.png" alt=""/>
+            </object><br/>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h4>Basé sur ce modèle original :</h4>
+           
+                <img src="Contenu/images/table1.png" alt=""/><br/>
+               
+        </td>
+    </tr>
+</table>
